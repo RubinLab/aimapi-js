e82068bd01a82d3049d4027e3e04e691316d3fd7
Emel Alkim <emelalkim@msn.com>
2021-04-01 14:27:55 -0700

    add support for question type code

--- src/conversion.js
+++ src/conversion.js
@@ -55,6 +55,23 @@ function epadCD2SRCD(typecode) {
     CodeMeaning: typecode["iso:displayName"].value,
   };
 }
+// obj can be observation entity or observation entity characteristic
+// TODO what about physical entity characteristic
+function createQuantitativeEvaluationWQuestionTypeCode(obj, upperQuestion) {
+  // we need typecode and either question type or upper question (observation for characteristic)
+  if (obj.typeCode && (obj.questionTypeCode || upperQuestion)) {
+    // if the obj does not have question type use the imaging observation
+    const conceptName = epadCD2SRCD(obj.questionTypeCode ? obj.questionTypeCode[0]: upperQuestion[0]);
+    const concept = epadCD2SRCD(obj.typeCode[0]);
+    return {
+      RelationshipType: "CONTAINS",
+      ValueType: "CODE",
+      ConceptNameCodeSequence: conceptName,
+      ConceptCodeSequence: concept,
+    };
+  } 
+  return null;
+}
 
 // TODO handle observation characteristic under physical entity
 function getQuantitativeEvaluations(aim) {
@@ -64,19 +81,47 @@ function getQuantitativeEvaluations(aim) {
   if (!imagingObservations) return [];
   const quantitativeEvaluations = [];
   imagingObservations.ImagingObservationEntity.forEach((io) => {
-    const conceptName = epadCD2SRCD(io.typeCode[0]);
-    if (io.imagingObservationCharacteristicCollection) {
-      io.imagingObservationCharacteristicCollection.ImagingObservationCharacteristic.forEach(
-        (ioc) => {
-          const concept = epadCD2SRCD(ioc.typeCode[0]);
-          quantitativeEvaluations.push({
-            RelationshipType: "CONTAINS",
-            ValueType: "CODE",
-            ConceptNameCodeSequence: conceptName,
-            ConceptCodeSequence: concept,
-          });
-        }
-      );
+    // we have the question type, make question type question, typecode as answer
+    // assuming if one has it all does
+    if (io.questionTypeCode) {
+      const qeIO = createQuantitativeEvaluationWQuestionTypeCode(io);
+      if (qeIO) quantitativeEvaluations.push(qeIO);
+      else console.error('Could not generate quantitative evaluation entity for imaging observation', 'label:', io.label.value, 'questionTypeCode:', io.questionTypeCode, 'typeCode:', io.typeCode );
+      if (io.imagingObservationCharacteristicCollection) {
+        io.imagingObservationCharacteristicCollection.ImagingObservationCharacteristic.forEach(
+          (ioc) => {
+            // this is just for nominal
+            // TODO how about others
+            // TODO it is no coded term need to figure out the correct way to put in sr
+            // if I put it in concept I get this
+            // Error - Missing attribute Type 1 Required Element=<NumericValue> Module=<NumericMeasurementMacro>
+            // const val = (ioc.characteristicQuantificationCollection) ? 
+            //   ioc.characteristicQuantificationCollection.CharacteristicQuantification[0].value.value
+            //   : 
+            //   undefined;
+            // still supporting ioc with no question type
+            const qeIOC = createQuantitativeEvaluationWQuestionTypeCode(ioc, io.questionTypeCode || io.typeCode);
+            if (qeIOC) quantitativeEvaluations.push(qeIOC);
+            else console.error('Could not generate quantitative evaluation entity for imaging observation characteristic', 'label:', ioc.label.value, 'questionTypeCode:', ioc.questionTypeCode, 'typeCode:', ioc.typeCode, 'io questionTypeCode:', io.questionTypeCode, 'io typeCode:', io.typeCode );
+     
+          }
+        );
+      }
+    } else { // fallback for old aims
+      const conceptName = epadCD2SRCD(io.typeCode[0]);
+      if (io.imagingObservationCharacteristicCollection) {
+        io.imagingObservationCharacteristicCollection.ImagingObservationCharacteristic.forEach(
+          (ioc) => {
+            const concept = epadCD2SRCD(ioc.typeCode[0]);
+            quantitativeEvaluations.push({
+              RelationshipType: "CONTAINS",
+              ValueType: "CODE",
+              ConceptNameCodeSequence: conceptName,
+              ConceptCodeSequence: concept,
+            });
+          }
+        );
+      }
     }
   });
   return quantitativeEvaluations;
@@ -120,8 +165,7 @@ function generateMetadataProviderAndToolState(aim) {
     markups = linesToPerpendicular(markups);
     markups.forEach((markup) => {
       const tool = createTool(markup);
-      tool.data.identifier = aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0].name
-      .value; // add type to the end? or shape uid
+      tool.data.identifier = aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0].name.value; // add type to the end? or shape uid
       // template
       tool.data.finding = epadCD2SRCD(
         aim.ImageAnnotationCollection.imageAnnotations.ImageAnnotation[0]
