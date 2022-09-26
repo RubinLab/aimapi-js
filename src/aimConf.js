833620bbdaa8fe90aacc53fbb406c2eefe4179d0
Mete Akdogan <muakdogan@gmail.com>
2022-09-26 14:50:46 -0700

    bumps to version 4_3

--- src/aimConf.js
+++ src/aimConf.js
@@ -1,5 +1,5 @@
 export default {
-  aimVersion: "AIMv4_2",
+  aimVersion: "AIMv4_3",
   "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
   "xmlns:rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
   xmlns: "gme://caCORE.caCORE/4.4/edu.northwestern.radiology.AIM",
