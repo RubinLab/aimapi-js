9ef84db7ee003878238fb3820b1ac53ed2f4d611
OzgeYurtsever <ozge.ikiz.yurtsever@gmail.com>
2023-06-23 15:00:43 -0700

    Rebuild the lib

--- build/aimapi.js
+++ build/aimapi.js
@@ -1232,7 +1232,7 @@
   }
 
   function getSingleImageData(image) {
-    consolelog(" ====> image in aim api getSingleImageData");
+    console.log(" ====> image in aim api getSingleImageData");
     console.log(image);
     return {
       sopClassUid: image.data?.string("x00080016") || image.metadata?.x00080016 || "",
